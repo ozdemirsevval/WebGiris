540c399f74e9d5692e7df47868f8b2a62f01e768
ozdemirsevval <sevvalozdemir2002@windowslive.com>
2024-10-07 14:06:42 +0300

    first

---
+++ WebMVC/Controllers/PersonelController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthwindEntities.SqlServerEntities;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class PersonelController : Controller
+    {
+        private readonly SqlNorthwinContext dbContext;
+
+        public PersonelController(SqlNorthwinContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            PersonelInsertVM personelInsertVM = new PersonelInsertVM();
+            return  View(PersonelInsertVM);
+        }
+    }
+}
---
+++ WebMVC/Models/PersonelInsertVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMVC.Models
+{
+    public class PersonelInsertVM
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Kullanıcı Kodu Zorunlu Alandır.")]
+        [MinLength(4,ErrorMessage ="En Az Dört Hane Olamılıdır.")]
+        [MaxLength(7,ErrorMessage ="En Fazla Yedi Karakter Olmalıdır.")]
+        
+        public string KullaniciAdi { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Kullanıcı Kodu Zorunlu Alandır.")]
+        [MinLength(4, ErrorMessage = "En Az Dört Hane Olamılıdır.")]
+        [MaxLength(7, ErrorMessage = "En Fazla Yedi Karakter Olmalıdır.")]
+        public string Password { get; set; }
+
+    }
+}
